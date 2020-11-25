6e323822009a8f4244ea07ab69578c2bd8c49fd8
Alex <amw22@indiana.edu>
2020-11-25 17:22:34 -0600

    initial django commit

---
+++ eve_reactor/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for eve_reactor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eve_reactor.settings')
+
+application = get_asgi_application()

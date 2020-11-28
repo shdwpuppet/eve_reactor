cb0d10f479d3d4c14c8b0058e0f95655b2b45d8d
shdwpuppet <shdw.puppet@gmail.com>
2020-11-28 02:51:42 -0600

    staticfiles fuckery

--- eve_reactor/settings/local.py
+++ eve_reactor/settings/local.py
@@ -19,7 +19,7 @@ DATABASES = {
 SHARE_URL = "http://127.0.0.1:8000"
 # Static assets
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static', 'static_dirs'),
 )
